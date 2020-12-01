80b202c94724b02e96fc312cbb68039a1001ef46
Unwana Essien <unwana.essien@outlook.com>
2020-12-01 13:55:12 +0100

    Updates

---
+++ precache-manifest.2969a0052729ca2521c8f8d6e148a1d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb14357785827ff7cf16d277c19c3d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7acb7c4fc5c65c2d2a75",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3376afa8bdd92cd3233f",
+    "url": "/static/js/2.1d1c863f.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.1d1c863f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7acb7c4fc5c65c2d2a75",
+    "url": "/static/js/main.0b02449b.chunk.js"
+  },
+  {
+    "revision": "8b4795dfec10103fdcbe",
+    "url": "/static/js/runtime-main.8a947f7d.js"
+  }
+]);
